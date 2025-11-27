0e3fa76a90743d175c9533d9795ab6aa36b7dcfc
Joao Gabriel Pereira Guzzo <joao.guzzo@estudadnte.ufjf.br>
2025-11-27 11:26:45 -0300

    criei funcoes e strcts para melhor organização do codigo

--- jogo.cpp
+++ jogo.cpp
@@ -6,66 +6,95 @@ int count;
 int tentativas = 1;
 int numeroJogador;
 
-void jogoDaSenha(){
-   //criar funcao randomizador de senha
+//Criei um struct q randomiza a senha para maior organizacao do codigo;
+struct senhaRandom{
     int digito1 = (rand() % 6) + 1;
     int digito2 = (rand() % 6) + 1;
     int digito3 = (rand() % 6) + 1;
     int digito4 = (rand() % 6) + 1;
     int numeroFinal = digito1 * 1000 + digito2 * 100 + digito3 * 10 + digito4;
+};
 
+//struct para tratar o numero jogado pelo usuario
+struct numeroJogado
+{
+     int primeiroDigitoJ;
+     int segundoDigitoJ;
+     int terceiroDigitoJ;
+     int quartoDigitoJ;
+     
+};
+
+
+//funcao q trata o numero jogado pelo usuario, separando os digitos
+numeroJogado trataNumeroJogador(int numeroJogador){
+
+    numeroJogado njr;
+    njr.primeiroDigitoJ = numeroJogador / 1000;
+    njr.segundoDigitoJ = (numeroJogador - njr.primeiroDigitoJ *1000)/ 100;
+    njr.terceiroDigitoJ = (numeroJogador - (njr.primeiroDigitoJ * 1000 + njr.segundoDigitoJ * 100))/10;
+    njr.quartoDigitoJ = numeroJogador - (njr.primeiroDigitoJ * 1000 + njr.segundoDigitoJ * 100 + njr.terceiroDigitoJ * 10);
+
+    return njr;
+
+}
+
+
+
+
+void jogoDaSenha(){
+   
+    //chama as variaveis do struct
+    senhaRandom senhaRandom;
     
 
     int numeroJogador;
     cin >> numeroJogador;
     
     
-    if (numeroJogador == numeroFinal){
+    if (numeroJogador == senhaRandom.numeroFinal){
         cout << "Você ganhou!";
     } else {
        
         while(tentativas <= 10){
                 while(count <= 4 ){
                     
-                    int primeiroDigitoJ = numeroJogador / 1000;
-                    int segundoDigitoJ = (numeroJogador - primeiroDigitoJ *1000)/ 100;
-                    int terceiroDigitoJ = (numeroJogador - (primeiroDigitoJ * 1000 + segundoDigitoJ * 100))/10;
-                    int quartoDigitoJ = numeroJogador - (primeiroDigitoJ * 1000 + segundoDigitoJ * 100 + terceiroDigitoJ * 10);
-                    
+                    numeroJogado nj = trataNumeroJogador(numeroJogador);
+
                     if(tentativas < 10){
                         
-                        if(numeroJogador == numeroFinal){
+                        if(numeroJogador == senhaRandom.numeroFinal){
                             cout << "voce venceu!";
                             break;
                         } else{    
                             //verifica se o primeiro digito e compativel com qualquer um dos 4 digitos da senha
-                            if (primeiroDigitoJ == digito1)
+                            if (nj.primeiroDigitoJ == senhaRandom.digito1)
                             {
                                 cout << "o";
                                 
-                            } else if(primeiroDigitoJ == digito2 or primeiroDigitoJ == digito3 or primeiroDigitoJ == digito4){
+                            } else if(nj.primeiroDigitoJ == senhaRandom.digito2 or nj.primeiroDigitoJ == senhaRandom.digito3 or nj.primeiroDigitoJ == senhaRandom.digito4){
                                 cout << "x";
                             }else{
                                 cout << "_";
                             }
                             
                             //verifica se o segundo digito e compativel com qualquer um dos 4 digitos da senha
-                            if(segundoDigitoJ == digito2){
+                            if(nj.segundoDigitoJ == senhaRandom.digito2){
                                 cout << "o";
                                
-                            }else if (segundoDigitoJ == digito1 or segundoDigitoJ == digito3 or segundoDigitoJ == digito4)
+                            }else if (nj.segundoDigitoJ == senhaRandom.digito1 or nj.segundoDigitoJ == senhaRandom.digito3 or nj.segundoDigitoJ == senhaRandom.digito4)
                             {
                                 cout << "x";
                             }else{
                                 cout << "_";
                             }
         
-        
+
                             //verifica se o terceiro digito e compativel com qualquer um dos 4 digitos da senha
-                            if(terceiroDigitoJ == digito3){
+                            if(nj.terceiroDigitoJ == senhaRandom.digito3){
                                 cout << "o";
                                 
-                            }else if (terceiroDigitoJ == digito1 or terceiroDigitoJ == digito2 or terceiroDigitoJ == digito4)
+                            }else if (nj.terceiroDigitoJ == senhaRandom.digito1 or nj.terceiroDigitoJ == senhaRandom.digito2 or nj.terceiroDigitoJ == senhaRandom.digito4)
                             {
                                 cout << "x";
                             }else{
@@ -73,10 +102,10 @@ void jogoDaSenha(){
                             }
         
                             //verifica se o quarto digito e compativel com qualquer um dos 4 digitos da senha
-                            if(quartoDigitoJ == digito4){
+                            if(nj.quartoDigitoJ == senhaRandom.digito4){
                                 cout << "o" << endl;
                                
-                            }else if (quartoDigitoJ == digito1 or quartoDigitoJ == digito2 or quartoDigitoJ == digito3)
+                            }else if (nj.quartoDigitoJ == senhaRandom.digito1 or nj.quartoDigitoJ == senhaRandom.digito2 or nj.quartoDigitoJ == senhaRandom.digito3)
                             {
                                 cout << "x" << endl;
                             }else{
